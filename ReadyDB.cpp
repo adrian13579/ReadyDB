3d6bdfa62e91393ffaa576cc4dffd03126fb1f43
adrianportales135 <adrianportales135@gmail.com>
2024-06-25 23:01:55 -0400

    Add entry point for application

---
+++ ReadyDB.cpp
@@ -0,0 +1,20 @@
+﻿// ReadyDB.cpp : Defines the entry point for the application.
+//
+
+#include "ReadyDB.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+
+	int a = 12;
+
+	int* b = &a;
+
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl;
+	
+	return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,20 @@
+﻿// main.cpp : Defines the entry point for the application.
+//
+
+#include "main.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+
+	int a = 12;
+
+	int* b = &a;
+
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl;
+	
+	return 0;
+}
